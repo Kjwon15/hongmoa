57a8d722205dd7eb0d4c66787b3c6176ed94b5cc
kjwon15 <kjwonmail@gmail.com>
2015-08-26 16:06:06 +0900

    Add python module

---
+++ apps/python.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from decorators import on_command
+
+from contextlib import closing
+from StringIO import StringIO
+import sys
+
+
+@on_command(['py', 'python'])
+def run(robot, channel, tokens):
+    '''Python 스크립트를 실행 후 결과 출력'''
+    code = ' '.join(tokens)
+    with closing(StringIO()) as sout:
+        sys.stdout = sout
+
+        exec code in {'__builtins__': {}}, {}
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        r_out = sout.getvalue()
+
+    return r_out
--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 
-APPS = ['hello_world', 'helper']
+APPS = ['hello_world', 'helper', 'python']
 
-SLACK_TOKEN = None
+SLACK_TOKEN = 'xoxb-8137707063-GxNA5NerpmqnhJxD2GVFq7FU'
 
 REDIS_URL = None
