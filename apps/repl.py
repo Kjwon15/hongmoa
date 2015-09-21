d6d1236ec804be59191ed23b51ddf13e3c90cc6a
kjwon15 <kjwonmail@gmail.com>
2015-09-21 17:01:39 +0900

    Add repl module

---
+++ apps/repl.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from decorators import on_command
+
+from tempfile import NamedTemporaryFile
+import os
+import subprocess
+
+
+@on_command(['repl'])
+def run(robot, channel, tokens):
+    '''스크립트를 실행 후 결과 출력'''
+    code = ' '.join(tokens).strip()
+
+    script_file = NamedTemporaryFile(delete=True)
+
+    with open(script_file.name, 'w') as fp:
+        fp.write(code)
+
+    os.chmod(script_file.name, 0755)
+    script_file.file.close()
+
+    process = subprocess.Popen(
+        [script_file.name],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    process.wait()
+    stdout, stderr = process.communicate()
+
+    return stderr or stdout
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 
-APPS = ['hello_world', 'helper', 'python']
+APPS = ['hello_world', 'helper', 'python', 'repl']
 
 SLACK_TOKEN = 'xoxb-8137707063-GxNA5NerpmqnhJxD2GVFq7FU'
 
