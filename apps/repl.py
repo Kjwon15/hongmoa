cbe9dc4afb83806a26bb8c94d5578bced9a4a4b6
kjwon15 <kjwonmail@gmail.com>
2016-04-05 16:46:37 +0900

    Get stdin input for repl

--- apps/repl.py
+++ apps/repl.py
@@ -10,7 +10,11 @@ import subprocess
 @on_command(['repl'])
 def run(robot, channel, tokens):
     '''스크립트를 실행 후 결과 출력'''
-    code = ' '.join(tokens).strip()
+    code = tokens[0].strip()
+    try:
+        stdin = tokens[1]
+    except IndexError:
+        stdin = None
 
     script_file = NamedTemporaryFile(delete=True)
 
@@ -23,9 +27,14 @@ def run(robot, channel, tokens):
     process = subprocess.Popen(
         [script_file.name],
         env={},
+        stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
+    if stdin:
+        process.stdin.write(stdin)
+        process.stdin.close()
     process.wait()
-    stdout, stderr = process.communicate()
+    stdout = process.stdout.read()
+    stderr = process.stderr.read()
 
     return stderr or stdout
